68278484e80a73275ebbe5147cffcf4b1cb56f2b
igorcamposdeborba <igor_campos5@hotmail.com>
2024-12-22 21:48:36 -0300

    fixed issue

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -37,7 +37,7 @@ const Signup = () => {
 
         try {
             const result = await signup(firstName, lastName, email, password);
-            
+
             if (typeof result === "string") {
                 setError(result);
                 return;
